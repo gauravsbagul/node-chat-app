2d1cd3f823014d23126fbd2c955177f71a1966c3
Gaurav Bagul <gauravsbagul@gmail.com>
2020-06-06 16:12:45 +0530

    Added / route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static(publicPath));
+app.get("/", (req, res) => {
+  res.send("Welcome to heroku chat app");
+});
 
 app.listen(port, () => {
   console.log("TCL:: server is up at", port);
