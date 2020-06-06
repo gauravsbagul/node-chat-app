cf7ad11f56eafb50a6ec6e0e88500cbd7dc64471
Gaurav Bagul <gauravsbagul@gmail.com>
2020-06-06 17:21:45 +0530

    Added socket

--- server/server.js
+++ server/server.js
@@ -1,17 +1,26 @@
 /** @format */
 const path = require("path");
+const http = require("http");
+
 const express = require("express");
+const socketIO = require("socket.io");
+
 const publicPath = path.join(__dirname, "../public");
 
 const port = process.env.PORT || 3000;
-
 const app = express();
 
-app.use(express.static(publicPath));
-app.get("/", (req, res) => {
-  res.send("Welcome to heroku chat app");
-});
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port, () => {
-  console.log("TCL:: server is up at", port);
+app.use(express.static(publicPath));
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+  socket.on("disconnect", () => {
+    console.log("Disconnected form server");
+  });
+});
+server.listen(port, () => {
+  console.log("server is up on", port);
 });
